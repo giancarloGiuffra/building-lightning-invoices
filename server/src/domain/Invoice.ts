a412c473b29dbd5dcbd67a67f5a1343db935e644
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2023-05-20 13:27:45 +0200

    Implement invoice memo creation

--- server/src/domain/Invoice.ts
+++ server/src/domain/Invoice.ts
@@ -13,7 +13,7 @@ export class Invoice {
      * @returns
      */
     public static createMemo(linkId: string, buyer: string) {
-        throw new Error("Exercise! Replace me to pass tests!");
+        return `buy_${linkId}_${buyer}`;
     }
 
     /**
